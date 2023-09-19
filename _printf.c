ab5adbda10b80242c9ec685d083f4f4796cd4d71
Vikiey <ohaivicky00@gmail.com>
2023-09-19 09:35:27 -0700

    kool

--- _printf.c
+++ _printf.c
@@ -3,16 +3,17 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - It prints the functions
+ * _printf - produces output according to a format
  * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int i;
+	char *st;
+	char c;
+	int count = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -21,31 +22,39 @@ int _printf(const char *format, ...)
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			i++;
+			if (format[i] == '\0')
+				break;
+		}
+		else if (format[i] == 'c')
+		{
+			c = va_arg(list, int);
+			_putchar(c);
+			count++;
+		}
+		else if (format[i] == 's')
+		{
+			st = va_arg(list, char *);
+			while (*st)
+			{
+				_putchar(*st);
+				st++;
+				count++;
+			}
+		}
+		else if (format[i] == '%')
+		{
+			_putchar('%');
+			count++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			_putchar(format[i]);
+			count++;
 		}
 	}
-
-	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
